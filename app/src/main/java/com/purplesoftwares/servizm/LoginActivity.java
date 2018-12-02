a09a459b4fe21270ec8e9bd863842c29c77e025a
servizm <servizmapp@gmail.com>
2018-12-03 01:20:05 +0300

    initial commit

--- app/src/main/java/com/purplesoftwares/servizm/LoginActivity.java
+++ app/src/main/java/com/purplesoftwares/servizm/LoginActivity.java
@@ -1,39 +1,176 @@
 package com.purplesoftwares.servizm;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnFacebookLogin;
-    private Button btnLogout;
+    private CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private TextView txtUser;
+    private TextView txtEmail;
+    private ImageView imgProfile;
+    private LoginButton logoutButton;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.purplesoftwares.servizm",   // Package name
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures)
+            {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {  }
+        catch (NoSuchAlgorithmException e) {        }
+
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+        txtUser = (TextView) findViewById(R.id.txtUser);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        imgProfile = (ImageView) findViewById(R.id.imgProfile);
+        firebaseAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
 
-        btnFacebookLogin = findViewById(R.id.btnFacebookLogin);
-        btnLogout = findViewById(R.id.btnLogout);
+        loginButton = (LoginButton) findViewById(R.id.btnFacebookIn);
+        logoutButton = (LoginButton) findViewById(R.id.btnFacebookOut);
+        logoutButton.setOnClickListener(this);
 
 
-        btnFacebookLogin.setOnClickListener(new View.OnClickListener() {
+        loginButton.setReadPermissions("email", "public_profile");//user_status, publish_actions..
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Selam",Toast.LENGTH_SHORT).show();
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Sign In canceled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
 
-
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (AccessToken.getCurrentAccessToken() != null)
+                    Toast.makeText(LoginActivity.this, AccessToken.getCurrentAccessToken().getExpires().toString(), Toast.LENGTH_SHORT).show();
+                if (user != null) {
+                    String email = user.getEmail();
+                    String userName = user.getDisplayName();
+                    txtEmail.setText(email);
+                    txtUser.setText(userName);
+                    Picasso.get().load(user.getPhotoUrl()).into(imgProfile);
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+                } else {
+                    Log.d("TG", "SIGNED OUT");
+                    txtEmail.setText("");
+                    txtUser.setText("");
+                    imgProfile.setImageBitmap(null);
+                    loginButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+                }
+            }
+        };
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,
+                resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        firebaseAuth.signOut();
+        LoginManager.getInstance().logOut();
+    }
 }
+
+
+
+
+
+
