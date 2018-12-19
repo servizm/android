fc9223f97ce007648b22619bf0d115788b66abdc
servizm <servizmapp@gmail.com>
2018-12-20 01:57:21 +0300

    ...

--- app/src/main/java/com/purplesoftwares/servizm/FirstLoginPage.java
+++ app/src/main/java/com/purplesoftwares/servizm/FirstLoginPage.java
@@ -1,18 +1,37 @@
 package com.purplesoftwares.servizm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.TextView;
 
 
 public class FirstLoginPage extends AppCompatActivity {
+
+    private TextView txtPhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_login_page);
 
-    }
+        txtPhoneNumber = findViewById(R.id.txtPhoneNumber);
+
+        txtPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage();
+            }
+        });
 
     }
+
+    private void openPage() {
+        Intent mainIntent = new Intent(FirstLoginPage.this, SendSmsActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+}
 
---
+++ app/src/main/java/com/purplesoftwares/servizm/SendSmsActivity.java
@@ -0,0 +1,13 @@
+package com.purplesoftwares.servizm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendSmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_sms);
+    }
+}
